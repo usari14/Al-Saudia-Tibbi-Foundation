ceb0302977fba6addb10eb429e7362a71e59c75a
usari14 <udogar583@gmail.com>
2024-11-22 17:55:59 +0500

    search2

--- src/DataSet/AllProducts.js
+++ src/DataSet/AllProducts.js
@@ -8,12 +8,12 @@ import fp6 from '../assets/fp6.png';
 
 
 export const AllProducts = [
-    {id: 1, name: 'Product 0', price: '10', image: fp1, rating: 0, stock: 50, category: 'Majoon', reviews: 0, benifits: "Increases masculine vigor and masculinity...", usage: "Take 1/2 teaspoon before breakfast...", ingredients: ['ing1', 'ing2'] },
-    {id: 2, name: 'Product 1', price: '11', image: fp2, rating: 1, stock: 50, category: 'Majoon', reviews: 0, benifits: "Increases masculine vigor and masculinity...", usage: "Take 1/2 teaspoon before breakfast...", ingredients: ['ing1', 'ing2'] },
-    {id: 3, name: 'Product 2', price: '12', image: fp3, rating: 2, stock: 50, category: 'Majoon', reviews: 0, benifits: "Increases masculine vigor and masculinity...", usage: "Take 1/2 teaspoon before breakfast...", ingredients: ['ing1', 'ing2'] },
-    {id: 4, name: 'Product 3', price: '13', image: fp4, rating: 3, stock: 50, category: 'Majoon', reviews: 0, benifits: "Increases masculine vigor and masculinity...", usage: "Take 1/2 teaspoon before breakfast...", ingredients: ['ing1', 'ing2'] },
-    {id: 5, name: 'Product 4', price: '14', image: fp5, rating: 4, stock: 50, category: 'Majoon', reviews: 0, benifits: "Increases masculine vigor and masculinity...", usage: "Take 1/2 teaspoon before breakfast...", ingredients: ['ing1', 'ing2'] },
-    {id: 6, name: 'Product 5', price: '15', image: fp6, rating: 5, stock: 50, category: 'Majoon', reviews: 0, benifits: "Increases masculine vigor and masculinity...", usage: "Take 1/2 teaspoon before breakfast...", ingredients: ['ing1', 'ing2'] }
+    {id: 0, name: 'Product 0', price: '10', image: fp1, rating: 0, stock: 50, category: 'Majoon', reviews: 0, benifits: "Increases masculine vigor and masculinity...", usage: "Take 1/2 teaspoon before breakfast...", ingredients: ['ing1', 'ing2'] },
+    {id: 1, name: 'Product 1', price: '11', image: fp2, rating: 1, stock: 50, category: 'Majoon', reviews: 0, benifits: "Increases masculine vigor and masculinity...", usage: "Take 1/2 teaspoon before breakfast...", ingredients: ['ing1', 'ing2'] },
+    {id: 2, name: 'Product 2', price: '12', image: fp3, rating: 2, stock: 50, category: 'Majoon', reviews: 0, benifits: "Increases masculine vigor and masculinity...", usage: "Take 1/2 teaspoon before breakfast...", ingredients: ['ing1', 'ing2'] },
+    {id: 3, name: 'Product 3', price: '13', image: fp4, rating: 3, stock: 50, category: 'Majoon', reviews: 0, benifits: "Increases masculine vigor and masculinity...", usage: "Take 1/2 teaspoon before breakfast...", ingredients: ['ing1', 'ing2'] },
+    {id: 4, name: 'Product 4', price: '14', image: fp5, rating: 4, stock: 50, category: 'Majoon', reviews: 0, benifits: "Increases masculine vigor and masculinity...", usage: "Take 1/2 teaspoon before breakfast...", ingredients: ['ing1', 'ing2'] },
+    {id: 5, name: 'Majoon', price: '15', image: fp6, rating: 5, stock: 50, category: 'Majoon', reviews: 0, benifits: "Increases masculine vigor and masculinity...", usage: "Take 1/2 teaspoon before breakfast...", ingredients: ['ing1', 'ing2'] }
 ];
 
 export default AllProducts;
